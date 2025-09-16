bf3eef2aef5f0319b029b074e19f6d1d50cc842d
Gabriel Porto <gabrielportocarrero2009@gmail.com>
2025-09-16 15:34:58 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,55 +1,87 @@
-class Locadora 
-    def __init__(sefl):
-        self.clientes = []
-        self.items = []
+class Locadora:
+    def __init__(self):
+        self.__clientes = []
+        self.__itens = []
+    
+    def cadastrarCliente(self,cliente):
+        self.__clientes.append(cliente)
 
-        def cadastradoCliente():
-            pass
+    def cadastrarItem(self, item):
+        self.__itens.append(item)
+    
+    def listarClientes(self):
+        return self.__clientes
 
-        def cadastradoItem():
-            pass
+    def listarItens(self):
+        return self.__itens
 
-        def listarClientes():
-            pass
-
-        def listarItems():
-            pass
-
- locadora_senai = Locadora()
+locadora_senai = Locadora()
 
 class Item:
-    def __init__(sefl):
-        self.codigo = len(listar)
-        self.titulo = titulo
-        self.disponivel = True 
+    def __init__(self, titulo):
+        self.__codigo = len(locadora_senai.listarItens()) + 1
+        self.__titulo = titulo
+        self.__disponivel = True
+    
+    def alugar(self):
+        self.__disponivel = False
 
-    def alugar():
-        pass
+    def devolver(self):
+        self.__disponivel = True
 
-    def devolver():
-        pass
+    def getCodigo(self):
+        return self.__codigo
+    
+    def getTitulo(self):
+        return self.__titulo
+    
+    def getDisponivel(self):
+        return self.__disponivel
 
-    class Jogo(Item):
-        def super().__init__(sefl, plataforma, faixa_etaria):
-            super().__init__(codigo, titulo, disponivel)
-            self._plataforma = plataforma
-            self._faixa_etaria = faixa_etaria
+class Jogo(Item):
+    def __init__(self, titulo, plataforma, faixa_etaria):
+        super().__init__(titulo=titulo)
+        self.__plataforma = plataforma
+        self.__faixa_etaria = faixa_etaria
 
-    class Filme(Item):
-        def __init__(self, titulo):
-            super().__init__(titulo=titulo)
-            self._genero = _genero
-            self._duracao = _duracao
+    def getPlataforma(self):
+        return self.__plataforma
+    
+    def getFaixa(self):
+        return self.__faixa_etaria
+    
+    
+class Filme(Item):
+    def __init__(self, titulo, genero, duracao):
+        super().__init__(titulo=titulo)
+        self.__genero = genero
+        self.__duracao = duracao
+    
+    def getGenero(self):
+        return self.__genero
+    
+    def getDuracao(self):
+        return self.__duracao
 
-    class Clientes:
-        def __init__(self, nome, cpf):
-            self.__nome = nome
-            self._cpf = cpf
-            self._itens_locados = []
+class Cliente():
+    def __init__(self, nome, cpf):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__itens_locados = []
 
-        def locar()
-            pass
-        def devolver()
-            pass
-        def listarItensLocados()
-            pass   
+    def locar(self, item):
+        item.alugar()
+        self.__itens_locados.append(item)
+
+    def devolver(self, item):
+        item.devolver()
+        self.__itens_locados.remove(item)
+
+    def listarItens(self):
+        return self.__itens_locados
+
+    def getNome(self):
+        return self.__nome
+    
+    def getCpf(self):
+        return self.__cpf
